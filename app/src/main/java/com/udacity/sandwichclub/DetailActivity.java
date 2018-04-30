bb0a8d685fbd83269c6545923bca87f9cb9baec0
Ben Snell <benjaminpaulsnell@gmail.com>
2018-04-29 20:13:06 -0600

    Format TextViews
    Format TextViews

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,6 +3,7 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
